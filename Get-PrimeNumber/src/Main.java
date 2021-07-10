41fc8b4d14239a9605378969fe3a46967335b4a0
Ryoichi246 <85016357+Ryoichi246@users.noreply.github.com>
2021-07-10 15:11:32 +0900

    Update Main.java

--- Get-PrimeNumber/src/Main.java
+++ Get-PrimeNumber/src/Main.java
@@ -5,6 +5,29 @@ public class Main {
 		// TODO 自動生成されたメソッド・スタブ
 //		Question 素数を抽出するプログラムを作成しましょう。パラメータはint型の配列で入力されます。
 //		要素数は入力者の任意です。
+		
+		ArrayList<Integer> seisuuList = new ArrayList<Integer>();
+	
+			for(int i = 1;i < 100;i++) {
+				seisuuList.add(i);
+			}
+		ArrayList<Integer> sosuuList = new ArrayList<Integer>();
+	
+			for(int i = 1;i < j ;i++){
+				for(int j = 1;j<i;j++) {
+					if (i % j != 0) {
+						sosuuList.add(i);
+						return;
+					}
+					
+				}
+				
+		
+		
+			}
+	
+//途中までの状態です、、、
+	
 	}
-
 }
+	
