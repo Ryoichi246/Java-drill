22c36a630ed7a225ae2a968a50177ca0be10f385
Ryoichi246 <85016357+Ryoichi246@users.noreply.github.com>
2021-07-16 15:43:31 +0900

    Update Main.java

--- Pyramids-MadeofStar/src/Main.java
+++ Pyramids-MadeofStar/src/Main.java
@@ -4,15 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 //		Qestion ９段の星（⭐️）で出来たピラミッドを作成しましょう。
-	for(int i=1 ; i<6 ; i++){
-		      for(int j=0; j<5-i; j++){
+		//ここで作成したい星の階段の段数を代入
+		int s = 9;
+		
+		for(int i=1 ; i<s+1 ; i++){
+		      for(int j=0; j<s-i; j++){
 		        System.out.print("  ");
 		      }
 		      for(int k=0; k<i*2-1; k++){
 		        System.out.print("☆");
 		      }
 		      System.out.println("");
-		 }
+		}
 	}
 
 }
